2b4bf073d63a12504ba0b43ea5280b2eb98170c3
sofiagarkot <44227901+sofiagarkot@users.noreply.github.com>
2019-11-16 15:12:17 +0200

    Update BaseArray.java

--- src/main/java/ua/edu/ucu/smartarr/BaseArray.java
+++ src/main/java/ua/edu/ucu/smartarr/BaseArray.java
@@ -1,10 +1,15 @@
 package ua.edu.ucu.smartarr;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-public class BaseArray implements SmartArray{
+
+public class BaseArray implements SmartArray {
 
     private ArrayList arr;
-    public BaseArray(Object[] arr){ this.arr = new ArrayList<>(Arrays.asList(arr)); }
+
+    public BaseArray(Object[] arr) {
+        this.arr = new ArrayList<>(Arrays.asList(arr));
+    }
 
     @Override
     public Object[] toArray() {
