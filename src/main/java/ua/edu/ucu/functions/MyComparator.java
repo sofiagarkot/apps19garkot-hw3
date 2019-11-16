98bca7d96b55f99fce427bc2f5912d15df1f040b
sofiagarkot <44227901+sofiagarkot@users.noreply.github.com>
2019-11-16 15:10:07 +0200

    Update MyComparator.java

--- src/main/java/ua/edu/ucu/functions/MyComparator.java
+++ src/main/java/ua/edu/ucu/functions/MyComparator.java
@@ -8,12 +8,11 @@ package ua.edu.ucu.functions;
 import java.util.Comparator;
 
 /**
- *
  * @author Andrii_Rodionov
  */
-public interface MyComparator extends Comparator<Object>{
+public interface MyComparator extends Comparator<Object> {
 
     @Override
     int compare(Object o1, Object o2);
-    
+
 }
